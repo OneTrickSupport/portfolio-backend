c39ff980000ea97e84e0d8d316b26682a04844d8
OneTrickSupport <kallenilros@gmail.com>
2026-05-22 20:33:02 +0200

    Lambda: drop premature app.ready() (was locking decorator API for awsLambdaFastify)

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,6 +2,5 @@ import awsLambdaFastify from "@fastify/aws-lambda";
 import { buildServer } from "./app.js";
 
 const app = await buildServer();
-await app.ready();
 
 export const handler = awsLambdaFastify(app);
